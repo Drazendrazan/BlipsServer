db7eda5bddc357e31b438951b50e674664007d11
Achraf Web <44878760+achrafhkdk@users.noreply.github.com>
2021-04-24 16:34:53 +0100

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,19 @@
+Config = {}
+
+Config.Map = {
+  {name="<FONT FACE = 'JF Flat'>".."~g~Families~w~".."ـﻟﺍ ﻉﺭﺎﺷ",color=2, id=378, x =-126.4, y =-1621.0, z =31.0},
+  {name="<FONT FACE = 'JF Flat'>".."~y~Vagos~w~".."ـﻟﺍ ﻉﺭﺎﺷ",color=46, id=378,x=318.5, y =-2026.4, z =20.61},
+  {name="<FONT FACE = 'JF Flat'>".."~p~Ballas~w~".."ـﻟﺍ ﻉﺭﺎﺷ",color=50, id=378,x=99.4, y =-1935.9, z =20.8},
+  {name="<FONT FACE = 'JF Flat'>".."ﺔﻤﺨﻔﻟﺍ ﺕﺍﺭﺎﻴﺴﻟﺍ ﺮﺠﺘﻣ",color=28, id=326,x=-796.44, y = -220.53 , z= 37.08},
+  {name="<FONT FACE = 'JF Flat'>".."ﺭﺎﻄﻘﻟﺍ ﺔﻄﺤﻣ",color=3 , id=532 , x= -829.42 , y=-113.4 , z= 37.58},
+  {name="<FONT FACE = 'JF Flat'>".."ﺕﺎﺟﺍﺭﺪﻟﺍ ﺀﺍﺮﻛ",color=46, id=559 , x= -754.91 , y= -77.23 , z= 41.76},
+  {name="<FONT FACE = 'JF Flat'>".."ﺕﺍﺭﻮﻃﻮﻤﻟﺍ ﺀﺍﺮﻛ",color=46 , id=226 , x=-1012.5, y=-2688.47, z=13.79 },
+  {name="<FONT FACE = 'JF Flat'>".."ﺕﺎﻨﺣﺎﺸﻟﺍ ﺀﺍﺮﻛ",color=46 , id=318 , x= -246.36 , y=6198.74 , z=31.49},
+  {name="<FONT FACE = 'JF Flat'>".."ﻝﻮﻜﺴﻴﺒﻟﺍ ﺀﺍﺮﻛ",color=46 , id=126 , x= -66.69 , y= -1165.68 , z= 25.98},
+  {name="<FONT FACE = 'JF Flat'>".."ﻡﻮﺠﻧ 3 ﻕﺪﻨﻓ",color=2 , id=475 ,  x= -1470.61 , y= -657.89 , z= 29.4},
+  {name="<FONT FACE = 'JF Flat'>".."ﻊﻴﺒﻠﻟ ﻰﻬﻘﻣ",color=2 , id=374 ,  x= -625.77 , y= 236.97 , z= 81.88},
+  {name="<FONT FACE = 'JF Flat'>".."ﻲﻤﻴﻣ ﻮﻨﻳﺯﺎﻛ",color=3 , id=439 , x= 757.15 , y= -557.49 , z= 305.85},
+  {name="<FONT FACE = 'JF Flat'>".."ﺎﻴﻠﻌﻟﺍ ﺔﻤﻜﺤﻤﻟﺍ",color=5 , id=305 , x= 327.68 , y= -1633.64 , z= 32.54},
+  {name="<FONT FACE = 'JF Flat'>".."ﺓﺎﻣﺎﺤﻣ ﺐﺘﻜﻣ",color=5 , id=498 , x= -233.76 , y= -918.82 , z= 32.31}
+}
+
